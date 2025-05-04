0f4f969a01cb7c41b94d588f0cf0511e2d1af9e0
sushree <sushr33nayak@gmail.com>
2025-05-05 01:09:50 +0530

    first commit before dvc

---
+++ mycode.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import os
+
+#create sample dataframes which have column names
+data={'Name':['Alice','Bob','Charlie'],
+       'Age':[25,30,34],
+       'City':['NY','Ohio','Chicago']}
+
+df=pd.DataFrame(data)
+data_dir='data'
+os.makedirs(data_dir,exist_ok=True)
+
+file_path=os.path.join(data_dir,'sample_data.csv')
+df.to_csv(file_path,index=False)
+print(f"CSV file saved to{file_path}")
